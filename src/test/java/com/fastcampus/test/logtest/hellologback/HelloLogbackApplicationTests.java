771cdfe6b01f4aacda537dea90563685b657901c
fastcamedu <fastcamedu@gmail.com>
2023-02-16 11:36:53 +0900

    Init Spring Boot Logging Sample Project

---
+++ src/test/java/com/fastcampus/test/logtest/hellologback/HelloLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.test.logtest.hellologback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
